ab30dcf1a97c50d0f0ce70ba6d54c3a7005ec79a
VictorCam <43551031+VictorCam@users.noreply.github.com>
2021-07-02 09:28:48 -0700

    trying to make work
    trying to make work

--- server/server_connection.js
+++ server/server_connection.js
@@ -1,30 +1,44 @@
-const mariadb = require('mariadb');
+// const mariadb = require('mariadb');
+const postgres = require('postgres')
 
-//give lowest privilage level when connecting to database
-const pool = mariadb.createPool({
-    port: '3302',
-    host: "localhost",
-    user: "user",
-    password: "test",
-    database: "mysql",
-    connectionLimit: 5
+const sql = postgres({
+  host: "localhost",
+  username: 'postgres',
+  password: 'test',
+  database: "postgres",
 })
 
-asyncFunction()
-
-async function asyncFunction() {
-  try {
-    var conn = await pool.getConnection();
-    const [res] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'hostname'");
-    console.log("v1", res);
-    const [res2] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'port'");
-    console.log("v2", res2)
-  }
-  catch (err) {
-    console.log("ended conn on err", err)
-  }
-
-  if(conn) return conn.end()
+async function test() {
+await sql`SELECT 1;`
 }
 
-module.exports = pool;
+test()
+
+//give lowest privilage level when connecting to database
+// const pool = mariadb.createPool({
+//     port: '3302',
+//     host: "localhost",
+//     user: "user",
+//     password: "test",
+//     database: "mysql",
+//     connectionLimit: 5
+// })
+
+// asyncFunction()
+
+// async function asyncFunction() {
+//   try {
+//     var conn = await pool.getConnection();
+//     const [res] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'hostname'");
+//     console.log("v1", res);
+//     const [res2] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'port'");
+//     console.log("v2", res2)
+//   }
+//   catch (err) {
+//     console.log("ended conn on err", err)
+//   }
+
+//   if(conn) return conn.end()
+// }
+
+// module.exports = pool;

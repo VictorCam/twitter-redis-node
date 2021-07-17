29e59d45708428682cba40ec3c71c532c32ef6aa
VictorCam <43551031+VictorCam@users.noreply.github.com>
2021-07-17 13:46:34 -0700

    postgres migration
    postgres migration

--- server/server_connection.js
+++ server/server_connection.js
@@ -1,49 +1,24 @@
-// const mariadb = require('mariadb');
 const postgres = require('postgres')
 
 const sql = postgres({
   host: "localhost",
   username: 'puser',
   password: 'test',
-  port: 5430
+  database: 'cc',
+  port: 5500
 })
 
-// alter role postgres with password 'test';
+//postgres temp passowrd: test
+//puser temp password: test
 
 async function test() {
 test = await sql`SELECT inet_server_addr()`
 console.log("test", test)
 test = await sql`SELECT * FROM pg_settings WHERE name = 'port'`
 console.log("test", test)
+test = await sql`SELECT * FROM public."USER" WHERE id = 1`
+console.log("test", test)
 }
 test()
-
-
-//give lowest privilage level when connecting to database
-// const pool = mariadb.createPool({
-//     port: '3302',
-//     host: "localhost",
-//     user: "user",
-//     password: "test",
-//     database: "mysql",
-//     connectionLimit: 5
-// })
-
-// asyncFunction()
-
-// async function asyncFunction() {
-//   try {
-//     var conn = await pool.getConnection();
-//     const [res] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'hostname'");
-//     console.log("v1", res);
-//     const [res2] = await conn.query("SHOW VARIABLES WHERE Variable_name = 'port'");
-//     console.log("v2", res2)
-//   }
-//   catch (err) {
-//     console.log("ended conn on err", err)
-//   }
-
-//   if(conn) return conn.end()
-// }
 
 // module.exports = pool;

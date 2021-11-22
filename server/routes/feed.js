169ad213cafc45de6dd9ed783fe3e9de1d0cf4f2
VictorCam <43551031+VictorCam@users.noreply.github.com>
2021-11-21 20:58:53 -0800

    feed [almost done]
    almost finished the feed endpoint

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -8,7 +8,7 @@ const Joi = require("joi")
 require("dotenv").config()
 
 //a route that sets a cookie with 15 userid's (we first need to pick a selection)
-router.get("/selection", check_token(), async (req, res) => {
+router.post("/selection", check_token(), async (req, res) => {
 
     //limit by 15
     var amount = parseInt(req.query.amount)
@@ -20,6 +20,7 @@ router.get("/selection", check_token(), async (req, res) => {
 
     if(!followid) return res.status(400).json({"error": "you don't have any followers"})
 
+    cookiearr = []
     result = []
     jdata = {}
     // console.log(followid)
@@ -45,78 +46,54 @@ router.get("/selection", check_token(), async (req, res) => {
             // console.log("if2", userdata[0])
             read = index-postl_index
         }
+
+        //DO COOOKIE STUFF HERE
+        if(postl_size-read != 0) {
+            cookiearr.push(followid[i], postl_size, read)
+        }
+
+        // cookiearr.push({"un": userdata[0], "i": userdata[1], "id": })
+
         // console.log("end", userdata[0])
         result.push({"username": userdata[0], "icon": userdata[1], "userid": followid[i], "read": read, "unread": postl_size-read})
     }
 
+    res.cookie("followers", cookiearr, { signed: true, httpOnly: true })
+
     res.status(200).json(result)
-})
-
-router.post("/selection", check_token(), async (req, res) => {
-
-    //limit by 15
-    var amount = parseInt(req.query.amount)
-    var page = parseInt(req.query.page)
-    var start = amount*page+page
-    var end = amount*((page+1))+page
-
-    var followid = await client.zrevrange(`following:${req.userid}`, start, end, "withscores")
-
-    if(!followid) return res.status(400).json({"error": "you don't have any followers"})
-
-    //set a secure signed cookie
-    res.cookie("followers", followid, { signed: true, httpOnly: true })
-
-    res.status(200).json({"status": "ok"})
-
 })
 
 router.get("/feed", check_token(), async (req, res) => {
     try {
+        //we get 15 posts from each user and userid data using
+        //for loop to get the userid's postl and userid data
+        //then we execute with a pipeline then store the result and do it again
+        //until no more data in cookie (users)
+        //we get the userid and icon of that user
+        //we remove a user when they have no more posts
+        //when the cookie is empty we will call the selection route again to get new users
+        var followers = req.signedCookies.followers
 
-        //make the feed elastic depend on the amount of users
-        //that DO have a post
-
-        //get the 25 array of users
-
-        //check if the user has a cookie named followers
-        var followers = req.cookies.followers
-
-        if(!followers) return res.status(400).json({"error": "you don't have any followers"})
-
-        //get 3 results from the smallest unix timestamp from sorted set of following
-        let following = await client.zrange("following:" + req.userid, 0, 4, "WITHSCORES")
-        // console.log("following", following)
-
-        //check if they have posts else we updated the zrange score (we only limit up to 15 posts)
-        let has_posts = await client.zrangebyscore(`postl:${following[0]}`, following[1], "+inf", "WITHSCORES", "LIMIT", 0, 15)
-
-        if (has_posts.length == 0) {
-            //we silently update the zrange score of that user and grab the next one
-            //DO NOT DELETE THIS
-            // await client.zadd("following:" + req.userid, Math.floor(Date.now()/1000), following[0])
-            //return nothing
-            return res.status(200).json({})
-        }
+        // console.log(followers)
 
         var posts = []
-        //get the posts from the users
-        for (let i = 0; i < has_posts.length; i++) {
-            let post = await client.hgetall(`post:${has_posts[i]}`)
-            posts.push(post)
+        for(var i = 0; i < followers.length; i+=3) {
+            var unread = followers[i+1]-followers[i+2]
+            console.log("unread", unread)
+            var postl = await client.zrange(`postl:${followers[i]}`, followers[i+2], followers[i+1], "WITHSCORES")
+            console.log("postl", postl)
+
+            var pipe = client.pipeline()
+            var userdata = await client.hmget(`userid:${followers[i]}`, "username", "icon")
+            for(var j = 0; j < postl.length; j+=2) {
+                pipe.hgetall(`post:${postl[j]}`)
+            }
+            var results = await pipe.exec()
+            console.log("RESULT", results)
+            console.log("\n\n")
+            delete results[0][0];
+            posts.push({"username": userdata[0], "icon": userdata[1], "userid": followers[i], "unread": unread, "posts": results})
         }
-
-        // console.log("test", has_posts[has_posts.length-1])
-
-        //DO NOT DELETE THIS
-        // await client.zadd("following:" + req.userid, has_posts[has_posts.length-1], following[0])
-
-
-        // //get the posts that are greater than the last unix id
-        // let posts = await client.zrangebyscore(`postl:${following[0]}`, 0, following[1], "WITHSCORES")
-
-        // console.log("posts", posts)
-
 
         return res.status(200).json(posts)
 

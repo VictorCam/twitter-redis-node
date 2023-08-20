ee69f4bbc8f1986e86193b77bb8eddf953eff02e
VictorCam <43551031+VictorCam@users.noreply.github.com>
2023-08-20 18:32:47 -0500

    Delete client directory

--- client/playwright.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-const config = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 2020
-	}
-};
-
-export default config;
